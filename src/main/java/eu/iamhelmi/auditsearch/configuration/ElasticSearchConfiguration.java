9ad071bb96a646e106787b19a15d3bc86c1cbbfc
Helmi Mahara <helmi.mahara@gmail.com>
2023-01-31 23:20:44 +0100

    improved code

--- src/main/java/eu/iamhelmi/auditsearch/configuration/ElasticSearchConfiguration.java
+++ src/main/java/eu/iamhelmi/auditsearch/configuration/ElasticSearchConfiguration.java
@@ -2,6 +2,7 @@ package eu.iamhelmi.auditsearch.configuration;
 
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,10 +13,16 @@ import co.elastic.clients.transport.rest_client.RestClientTransport;
 
 @Configuration
 public class ElasticSearchConfiguration {
+	
+	@Value(value = "${elasticsearch.server}")
+    private String elasticsearchServer;
+	
+	@Value(value = "${elasticsearch.port}")
+    private int elasticsearchPort;
 
 	@Bean
 	public RestClient getRestClient() {
-		RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200)).build();
+		RestClient restClient = RestClient.builder(new HttpHost(elasticsearchServer, elasticsearchPort)).build();
 		return restClient;
 	}
 
